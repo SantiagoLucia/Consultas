d68319036c3874cf4c19bf29a4e9fdcd0e4a9e88
SantiLP <santiluciaprivi@gmail.com>
2024-02-14 15:24:00 -0300

    cambios

--- EE/EE - historial operaciones.sql
+++ EE/EE - historial operaciones.sql
@@ -1,68 +1,68 @@
-SELECT
-EE.TIPO_DOCUMENTO || '-' || EE.ANIO || '-' || EE.NUMERO || '-' || EE.CODIGO_REPARTICION_ACTUACION || '-' || EE.CODIGO_REPARTICION_USUARIO AS NRO_EXPEDIENTE,
-TO_CHAR(EE.FECHA_CREACION, 'DD/MM/YYYY') AS FECHA_CARATULACION,
-TO_CHAR(EE.FECHA_CREACION, 'HH24:MI:SS') AS HORA_CARATULACION,
-EE.CODIGO_REPARTICION_USUARIO AS COD_REPARTICION_CARATULACION, 
-REP_CARATULA.NOMBRE_REPARTICION AS REPARTICION_CARATULACION, 
-MIN_CARATULA.CODIGO_REPARTICION AS COD_MINISTERIO_CARATULACION, 
-MIN_CARATULA.NOMBRE_REPARTICION AS MINISTERIO_CARATULACION, 
-HIST.TIPO_OPERACION,
-HIST.ORD_HIST AS ORDEN,
-HIST.ESTADO,
-TO_CHAR(HIST.FECHA_OPERACION, 'DD/MM/YYYY') AS FECHA_OPERACION,
-TO_CHAR(HIST.FECHA_OPERACION, 'HH24:MI:SS') AS HORA_OPERACION,
-TO_CHAR(HIST.FECHA_OPERACION, 'MONTH') AS MES_OPERACION,
+select
+ee.tipo_documento || '-' || ee.anio || '-' || ee.numero || '-' || ee.codigo_reparticion_actuacion || '-' || ee.codigo_reparticion_usuario as nro_expediente,
+to_char(ee.fecha_creacion, 'dd/mm/yyyy') as fecha_caratulacion,
+to_char(ee.fecha_creacion, 'hh24:mi:ss') as hora_caratulacion,
+ee.codigo_reparticion_usuario as cod_reparticion_caratulacion, 
+rep_caratula.nombre_reparticion as reparticion_caratulacion, 
+min_caratula.codigo_reparticion as cod_ministerio_caratulacion, 
+min_caratula.nombre_reparticion as ministerio_caratulacion, 
+hist.tipo_operacion,
+hist.ord_hist as orden,
+hist.estado,
+to_char(hist.fecha_operacion, 'dd/mm/yyyy') as fecha_operacion,
+to_char(hist.fecha_operacion, 'hh24:mi:ss') as hora_operacion,
+to_char(hist.fecha_operacion, 'month') as mes_operacion,
 
-COALESCE(
-(SELECT REGEXP_REPLACE((
-TRUNC((H2.FECHA_OPERACION - HIST.FECHA_OPERACION)) || ' DIAS ' ||
-TRUNC(MOD((H2.FECHA_OPERACION - HIST.FECHA_OPERACION) * 24, 24)) || ' HS ' ||
-TRUNC(MOD((H2.FECHA_OPERACION - HIST.FECHA_OPERACION) * 24 * 60, 60)) || ' MIN'
+coalesce(
+(select regexp_replace((
+trunc((h2.fecha_operacion - hist.fecha_operacion)) || ' dias ' ||
+trunc(mod((h2.fecha_operacion - hist.fecha_operacion) * 24, 24)) || ' hs ' ||
+trunc(mod((h2.fecha_operacion - hist.fecha_operacion) * 24 * 60, 60)) || ' min'
 ), '[^A-Z0-9- ]', '')
-FROM EE_GED.HISTORIALOPERACION H2
-WHERE H2.ID_EXPEDIENTE = HIST.ID_EXPEDIENTE AND H2.ORD_HIST = HIST.ORD_HIST -1) 
+from ee_ged.historialoperacion h2
+where h2.id_expediente = hist.id_expediente and h2.ord_hist = hist.ord_hist -1) 
 ,
-'0 DIAS 0 HS 0 MIN'
-) TIEMPO_ENTRE_OPERACIONES,
+'0 dias 0 hs 0 min'
+) tiempo_entre_operaciones,
 
-HIST.MOTIVO,
+hist.motivo,
 
---ORIGEN--
-HIST.USUARIO AS USUARIO_ORIGEN,
-HIST.SECTOR_USUARIO_ORIGEN,
-HIST.DESCRIPCION_SECTOR_ORIGEN,
-HIST.REPARTICION_USUARIO AS REPARTICION_USUARIO_ORIGEN,
-HIST.DESCRIPCION_REPARTICION_ORIGEN,
-HIST.CODIGO_JURISDICCION_ORIGEN AS CODIGO_MINISTERIO_ORIGEN,
-MIN_ORIGEN.NOMBRE_REPARTICION AS DESC_MINISTERIO_ORIGEN,
+--origen--
+hist.usuario as usuario_origen,
+hist.sector_usuario_origen,
+hist.descripcion_sector_origen,
+hist.reparticion_usuario as reparticion_usuario_origen,
+hist.descripcion_reparticion_origen,
+hist.codigo_jurisdiccion_origen as codigo_ministerio_origen,
+min_origen.nombre_reparticion as desc_ministerio_origen,
 
---DESTINO--
-HIST.USUARIO_SELECCIONADO,
-HIST.GRUPO_SELECCIONADO,
-REGEXP_REPLACE(HIST.DESTINATARIO,'[^A-Z0-9-#-]','') AS DESTINATARIO,
-HIST.CODIGO_SECTOR_DESTINO,
-HIST.DESCRIPCION_SECTOR_DESTINO,
-REGEXP_REPLACE(HIST.CODIGO_REPARTICION_DESTINO,'[^A-Z0-9-#-]','') AS CODIGO_REPARTICION_DESTINO,
-HIST.DESCRIPCION_REPARTICION_DESTIN AS DESC_REPARTICION_DESTINO,
-HIST.CODIGO_JURISDICCION_DESTINO AS CODIGO_MINISTERIO_DESTINO,
-MIN_DESTINO.NOMBRE_REPARTICION AS DESC_MINISTERIO_DESTINO,
+--destino--
+hist.usuario_seleccionado,
+hist.grupo_seleccionado,
+regexp_replace(hist.destinatario,'[^A-Z0-9-#-]','') as destinatario,
+hist.codigo_sector_destino,
+hist.descripcion_sector_destino,
+regexp_replace(hist.codigo_reparticion_destino,'[^A-Z0-9-#-]','') as codigo_reparticion_destino,
+hist.descripcion_reparticion_destin as desc_reparticion_destino,
+hist.codigo_jurisdiccion_destino as codigo_ministerio_destino,
+min_destino.nombre_reparticion as desc_ministerio_destino,
 
---TRATA--
-T.CODIGO_TRATA AS CODIGO_TRATA,
-T.DESCRIPCION AS TRATA
+--trata--
+t.codigo_trata as codigo_trata,
+t.descripcion as trata
 
-FROM EE_GED.EE_EXPEDIENTE_ELECTRONICO EE
-LEFT JOIN EE_GED.HISTORIALOPERACION HIST ON EE.ID = HIST.ID_EXPEDIENTE
-LEFT JOIN EE_GED.TRATA T ON EE.ID_TRATA = T.ID
-LEFT JOIN TRACK_GED.SADE_REPARTICION MIN_ORIGEN ON MIN_ORIGEN.CODIGO_REPARTICION = HIST.CODIGO_JURISDICCION_ORIGEN
-LEFT JOIN TRACK_GED.SADE_REPARTICION MIN_DESTINO ON MIN_DESTINO.CODIGO_REPARTICION = HIST.CODIGO_JURISDICCION_DESTINO
-LEFT JOIN TRACK_GED.SADE_REPARTICION REP_CARATULA ON EE.CODIGO_REPARTICION_USUARIO = REP_CARATULA.CODIGO_REPARTICION
-LEFT JOIN TRACK_GED.SADE_REPARTICION MIN_CARATULA ON REP_CARATULA.MINISTERIO = MIN_CARATULA.ID_REPARTICION
+from ee_ged.ee_expediente_electronico ee
+left join ee_ged.historialoperacion hist on ee.id = hist.id_expediente
+left join ee_ged.trata t on ee.id_trata = t.id
+left join track_ged.sade_reparticion min_origen on min_origen.codigo_reparticion = hist.codigo_jurisdiccion_origen
+left join track_ged.sade_reparticion min_destino on min_destino.codigo_reparticion = hist.codigo_jurisdiccion_destino
+left join track_ged.sade_reparticion rep_caratula on ee.codigo_reparticion_usuario = rep_caratula.codigo_reparticion
+left join track_ged.sade_reparticion min_caratula on rep_caratula.ministerio = min_caratula.id_reparticion
 
-WHERE 
-EXTRACT(YEAR FROM HIST.FECHA_OPERACION) = 2020 AND
-HIST.TIPO_OPERACION = 'Pase' AND
-HIST.CODIGO_JURISDICCION_ORIGEN != 'FDE' AND
-HIST.CODIGO_JURISDICCION_DESTINO = 'FDE'
+where 
+extract(year from hist.fecha_operacion) = 2020 and
+hist.tipo_operacion = 'Pase' and
+hist.codigo_jurisdiccion_origen != 'FDE' and
+hist.codigo_jurisdiccion_destino = 'FDE'
 
-ORDER BY HIST.EXPEDIENTE, HIST.ORD_HIST ASC;
+order by hist.expediente, hist.ord_hist asc;
