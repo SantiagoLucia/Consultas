85dc6f44749bf5ad19c6e4136f9af353acd27bff
SantiLP <santiluciaprivi@gmail.com>
2024-02-15 10:46:00 -0300

    .

--- EE/EE - gedos en expediente.sql
+++ EE/EE - gedos en expediente.sql
@@ -1,14 +1,15 @@
-SELECT
-    EE.ID ID_EXP,
-    EE.TIPO_DOCUMENTO || '-' || EE.ANIO || '-' || EE.NUMERO || '- -' || EE.CODIGO_REPARTICION_ACTUACION || '-' || EE.CODIGO_REPARTICION_USUARIO AS NRO_EXPEDIENTE,
-    GD.ID ID_GEDO,
-    GD.NUMERO GEDOS_ASOCIADOS,
-    TR.RESERVA TIPO_RESERVA
-FROM
-    EE_GED.EE_EXPEDIENTE_ELECTRONICO EE
-    INNER JOIN EE_GED.EE_EXPEDIENTE_DOCUMENTOS ED ON EE.ID = ED.ID
-    INNER JOIN EE_GED.DOCUMENTO D ON ED.ID_DOCUMENTO = D.ID
-    INNER JOIN GEDO_GED.GEDO_DOCUMENTO GD ON D.NUMERO_SADE = GD.NUMERO
-    INNER JOIN GEDO_GED.GEDO_TIPO_RESERVA TR ON GD.TIPORESERVA = TR.ID
-WHERE
-    EE.ID = 1569742;
+select
+    ee.id id_exp,
+    ee.tipo_documento||'-'||ee.anio||'-'||ee.numero||'- -'||
+		ee.codigo_reparticion_actuacion||'-'||ee.codigo_reparticion_usuario as nro_expediente,
+    gd.id id_gedo,
+    gd.numero gedos_asociados,
+    tr.reserva tipo_reserva
+from
+    ee_ged.ee_expediente_electronico ee
+    inner join ee_ged.ee_expediente_documentos ed on ee.id = ed.id
+    inner join ee_ged.documento d on ed.id_documento = d.id
+    inner join gedo_ged.gedo_documento gd on d.numero_sade = gd.numero
+    inner join gedo_ged.gedo_tipo_reserva tr on gd.tiporeserva = tr.id
+where
+    ee.id = 1569742;
