571bb8c8606d7e56e0e9151ddff6ffc20d765794
SantiLP <santiluciaprivi@gmail.com>
2024-08-21 10:51:52 -0300

    .

--- EE/EE - buzon expedientes reparticion.sql
+++ EE/EE - buzon expedientes reparticion.sql
@@ -1,20 +1,31 @@
-select
-regexp_substr(p.groupid_, '[^-]+') as reparticion,
-regexp_substr(p.groupid_, '[A-Z0-9#]+', 1, 2) as sector,
-ee.tipo_documento||'-'||ee.anio||'-'||ee.numero||'- -'||
-        ee.codigo_reparticion_actuacion||'-'||ee.codigo_reparticion_usuario as nro_expediente,
-to_char(ee.fecha_creacion, 'dd/mm/yyyy') as fecha_caratulacion,
-ee.descripcion as descripcion        
+SELECT 
+	jp.REPARTICION,
+	jp.SECTOR,
+	'EX'||'-'||ee.ANIO||'-'||ee.NUMERO||'- -'||
+	        'GDEBA'||'-'||ee.CODIGO_REPARTICION_USUARIO as NRO_EXPEDIENTE,
+	TO_CHAR(ee.FECHA_CREACION, 'dd/mm/yyyy') as FECHA_CARATULACION,
+	ee.DESCRIPCION        	
 
-from ee_ged.jbpm4_participation p
-inner join ee_ged.jbpm4_task t on p.task_ = t.dbid_
-inner join ee_ged.ee_expediente_electronico ee on t.execution_id_ = ee.id_workflow
+FROM 
+	(SELECT
+		regexp_substr(groupid_, '[^-]+') as REPARTICION,
+		regexp_substr(groupid_, '[^-]+', 1, 2) as SECTOR,
+		TASK_
+	FROM
+		EE_GED.JBPM4_PARTICIPATION
+	WHERE
+		regexp_substr(groupid_, '[^-]+') IN (
+    		'DESIV','DPTEIV','DPTCAYOIV','DPTRYCIV',
+    		'DACIV','DSOIV','DPTSAIV','DPTOCIV')	
+	) jp
 
-where regexp_substr(p.groupid_, '[^-]+') in (
-    'DESIV','DPTEIV','DPTCAYOIV','DPTRYCIV',
-    'DACIV','DSOIV','DPTSAIV','DPTOCIV'
-)
+LEFT JOIN
+	EE_GED.JBPM4_TASK jt ON jt.DBID_ = jp.TASK_
+	
+LEFT JOIN 
+	EE_GED.EE_EXPEDIENTE_ELECTRONICO ee ON ee.ID_WORKFLOW = jt.EXECUTION_ID_ 
 
-order by regexp_substr(p.groupid_, '[^-]+'),
-    regexp_substr(p.groupid_, '[A-Z0-9#]+', 1, 2),
-    ee.fecha_creacion asc
+ORDER BY
+	jp.REPARTICION,
+	jp.SECTOR	
+;
