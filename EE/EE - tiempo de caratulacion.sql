6b0d8b81d37b00838d9e62d901944f55e9bf829f
SantiLP <santiluciaprivi@gmail.com>
2024-08-23 08:22:58 -0300

    .

--- EE/EE - tiempo de caratulacion.sql
+++ EE/EE - tiempo de caratulacion.sql
@@ -1,22 +1,20 @@
 SELECT 
-    --ee.tipo_documento || '-' || ee.anio || '-' || ee.numero || '- -GDEBA-' || ee.codigo_reparticion_usuario AS EXPEDIENTE,
-    t.codigo_trata AS "CODIGO TRATA",
-    t.descripcion AS "DESCRIPCION",
-    --TO_CHAR(ee.fecha_creacion, 'YYYY/MM/DD') AS "FECHA CREACION",
-    --TO_CHAR(ee.fecha_modificacion, 'YYYY/MM/DD') AS "FECHA PASE A GUARDA",
-    COUNT(t.codigo_trata) AS "CANTIDAD EXPEDIENTES",
-    SUM(TRUNC(ee.fecha_modificacion - ee.fecha_creacion)) AS "TOTAL DIAS",
-    TRUNC(SUM(TRUNC(ee.fecha_modificacion - ee.fecha_creacion)) / COUNT(t.codigo_trata)) AS "PROMEDIO DIAS POR EXPEDIENTE" 
+    t.codigo_trata,
+    COUNT(t.codigo_trata) AS CANTIDAD_EXPEDIENTES,
+    ROUND(SUM(ee.fecha_modificacion - ee.fecha_creacion)) AS CANTIDAD_DIAS,
+    ROUND(AVG(ee.fecha_modificacion - ee.fecha_creacion)) AS PROMEDIO_DIAS
 
-FROM EE_GED.ee_expediente_electronico ee
-INNER JOIN EE_GED.historialoperacion ho ON ee.id = ho.id_expediente
-INNER JOIN EE_GED.trata t ON ee.id_trata = t.id
+FROM 
+	EE_GED.ee_expediente_electronico ee
+
+INNER JOIN 
+	EE_GED.trata t ON ee.id_trata = t.id
 
 WHERE 
-    ee.codigo_reparticion_usuario LIKE '%TESTGDEBA'
-    AND ho.estado = 'Guarda Temporal'
-    AND 'FECHA CREACION' is not NULL
-    AND 'FECHA PASE A GUARDA' is not NULL
+	ee.FECHA_CREACION > TRUNC(SYSDATE-365) 
+    AND ee.codigo_reparticion_usuario = 'TESTGDEBA'
+    AND ee.estado = 'Guarda Temporal'
 
-GROUP BY t.codigo_trata, t.descripcion
-ORDER BY t.codigo_trata ASC;
+GROUP BY 
+	t.codigo_trata
+;
